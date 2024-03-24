4c27e48632f03ccc9577c823c86d2bd465c5db2d
hristomarkov112 <146643476+hristomarkov112@users.noreply.github.com>
2024-03-24 20:03:37 +0200

    Add files via upload

---
+++ Objects And Classes - Lab/_3_BigFactorial.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class _3_BigFactorial {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        BigInteger result = BigInteger.valueOf(1);
+
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(result);
+
+    }
+}
