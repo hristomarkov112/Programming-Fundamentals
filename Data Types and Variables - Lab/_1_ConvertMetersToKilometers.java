e88382d3f52fed19e02a89cf88827be51caa608c
hristomarkov112 <146643476+hristomarkov112@users.noreply.github.com>
2024-03-24 19:40:40 +0200

    Update _1_ConvertMetersToKilometers.java

--- Data Types and Variables - Lab/_1_ConvertMetersToKilometers.java
+++ Data Types and Variables - Lab/_1_ConvertMetersToKilometers.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class _1_ConvertMetersToKilometers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = Integer.parseInt(sc.nextLine());
+
+        double total = a / 1000.0;
+
+        System.out.printf("%.2f", total);
+    }
+}
 
