12bdef28f57e85d6cdd8084409cb3f1fedec015c
hristomarkov112 <146643476+hristomarkov112@users.noreply.github.com>
2024-03-24 20:37:03 +0200

    Add files via upload

---
+++ Objects And Classes - Exercise/_6_OrderByAge/Main.java
@@ -0,0 +1,39 @@
+package _6_OrderByAge;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Person> peopleList = new ArrayList<>();
+        String data = scanner.nextLine();
+
+        while (!data.equals("End")) {
+            //data = "{name} {ID} {age}".split("\\s+") -> ["{name}", "{ID}", "{age}"]
+            String name = data.split("\\s+")[0]; //име
+            String id = data.split("\\s+")[1]; //ID
+            int age = Integer.parseInt(data.split("\\s+")[2]); //"10" -> 10
+
+            Person person = new Person(name, id, age);
+            peopleList.add(person);
+
+            data = scanner.nextLine();
+        }
+
+        //списък с хората
+        //1. сортирам по възраст (ascending order)
+        peopleList.sort(Comparator.comparing(Person::getAge));
+
+        //2. принтираме данните за всеки човек
+        for (Person person : peopleList) {
+            //"Stephan with ID: 524244 is 10 years old."
+            System.out.println(person.getName() + " with ID: " + person.getId() + " is " + person.getAge() + " years old.");
+        }
+    }
+}
